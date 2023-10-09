1223f1e56bad715b1eb94f420873853f02771a6b
Jasvinder-Kaur <79320574+Jasvinder-Kaur@users.noreply.github.com>
2023-10-09 23:35:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let scroll = document.querySelector(".gallery");
+
+let backBtn = document.querySelector("#backBtn");
+let nextBtn = document.querySelector("#nextBtn");
+
+scroll.addEventListener("wheel", (evt) => {
+    evt.prevetDefault();
+    scroll.style.scrollBehavior = "auto";
+
+    scroll.scrollLeft += evt.deltaY;
+});
+
+backBtn.addEventListener("click", ()=> {
+    scroll.style.scrollBehavior = "smooth";
+    scroll.scrollLeft -= 900;
+});
+
+nextBtn.addEventListener("click", ()=> {
+    scroll.style.scrollBehavior = "smooth";
+    scroll.scrollLeft += 900;
+});
+
+
